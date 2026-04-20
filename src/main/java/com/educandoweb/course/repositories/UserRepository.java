161dfd6c7a21734654f650f6e2ded92da7d2cce2
Rony Hélio <rony100lima100@gmail.com>
2026-04-20 15:49:03 -0300

    Order, Instant, ISO 8601 - PARTE 2

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.User;
-import org.hibernate.query.criteria.JpaDerivedRoot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
