dcada9be7f9d06f2e58941f81c0532ebf8b0d5f3
Rony Hélio <rony100lima100@gmail.com>
2026-04-23 08:37:33 -0300

    OrderStatus enum, entity category

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -4,6 +4,8 @@ package com.educandoweb.course.config;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.OrderStatus;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +26,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {  //classe instancia o banco de dados
+
+        Category cat1 = new Category("Electronics", null);
+        Category cat2 = new Category( "Books", null);
+        Category cat3 = new Category( "Computers", null);
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
---
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -0,0 +1,57 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+
+public class Category implements Serializable { //Essa classe pode ser transformada em bytes (serializada)
+    private static final long serialVersionUID = 1L; //ID de versão da classe
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Category(String name, Long id) {
+        super();
+        this.name = name;
+        this.id = id;
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
